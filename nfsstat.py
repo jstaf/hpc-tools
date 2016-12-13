cced813ca47e9768f4dc58fc9efa908c759ce004
Jeff Stafford <jeff.stafford@live.com>
2016-12-12 19:49:15 -0500

    writes csv

--- nfsstat.py
+++ nfsstat.py
@@ -6,45 +6,73 @@ import sys
 import re
 import argparse
 import subprocess
-import numpy as np
+import csv
 
 def main():
     parser = argparse.ArgumentParser(description="Get disk usage on all nodes and sum it up by user")
     parser.add_argument('--nodelist', default='nodes.txt', type=str, help='List of nodes to check')
-    parser.add_argument('--device', default=None, type=str, help='Check only usage on a specific device')
     argv = parser.parse_args()
     if not os.path.exists(argv.nodelist):
         sys.exit('Requires a list of nodes to check!')
 
+    # get node names
     nodes = []
     with open(argv.nodelist) as nodelist:
         for line in nodelist:
             nodes.append(line.strip())
 
+    # get stats for nfs traffic
     all_stats = []
     for node in nodes:
-        all_stats.extend(get_node_nfs(node))
+        traffic = get_node_nfs(node)
+        all_stats.append(concat_nfs_entry(node, traffic))
 
+    # write to csv
+    with open('nfs.csv') as out:
+        csvwrite = csv.writer(out)
+        csvwrite.writerow(['node', 'device', 'user', 
+            'rOps_s', 'rkB_s', 'rkB_op', 'rretrans', 'ravgRTT_ms', 'ravgexe_ms',
+            'wOps_s', 'wkB_s', 'wkB_op', 'wretrans', 'wavgRTT_ms', 'wavgexe_ms'])
+        csvwrite.writerows(all_stats)
 
 
 def get_node_nfs(node):
     """
     Get and parse a node's NFS traffic.
     """
-    ssh = subprocess.Popen(['ssh', node, '"/usr/sbin/nfsiostat"'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    nfsiostat = ssh.stdout.read().split('\n')    
-    
+    ssh = subprocess.Popen(['ssh', node, '"/usr/sbin/nfsiostat"'], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    nfsiostat = ssh.stdout.read().split('\n')
+
     # list of dictionaries with nfs stats
     entries = []
     entry = {}
+    nextline = ''
     for line in nfsiostat:
-        if line == '':
-            pass
-        elif 'mounted on' in line:
-            if not entry: # returns true if not empty
+        if 'mounted on' in line:
+            if entry: # returns true if not empty
+                # add last entry
                 entries.append(entry)
             entry = {}
             entry['device'] = line.split(':')[0]
-            entry['user'] = line.split
-
+            entry['user'] = re.findall('/(\w+):', line)[0]
+        elif 'read:' in line:
+            nextline = 'read'
+        elif 'write:' in line:
+            nextline = 'write'
+        elif nextline != '':
+            # ops/s, kB/s, kB/op, retrans, avg RTT (ms), avg exe (ms)
+            entry[nextline] = re.findall('[0-9]+\.*[0-9]*', line)
+            nextline = ''
     return entries
+
+
+def concat_nfs_entry(node, entries):
+    """
+    Parse apart NFS entries into something interpretable 
+    """
+    outlist = []
+    for entry in entries:
+        outlist.append([node, entry['device'], entry['user']] + entry['read'] + entry['write'])
+    return outlist
+
