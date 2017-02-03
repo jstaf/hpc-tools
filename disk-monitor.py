5afa5ae15b13eae7bc961ecb6f6016abdeb51cbf
Jeff Stafford <jeff.stafford@live.com>
2017-02-03 13:27:31 -0500

    basically a copy of df now

--- disk-monitor.py
+++ disk-monitor.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
+import re
 import psutil
+import pandas as pd
 
-sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+sizes = ['B', 'K', 'M', 'G', 'T', 'P']
 
 def main():
     parser = argparse.ArgumentParser(description='Get disk usage for a given partition (or all partitions)')
@@ -14,10 +17,43 @@ def main():
     argv = parser.parse_args()
 
     partitions = argv.partition
+    all_partitions = [diskpart.mountpoint for diskpart in psutil.disk_partitions()]
     if partitions is None:
-        partitions = [diskpart.device for diskpart in psutil.disk_partitions()]
-    print(partitions)
+        partitions = all_partitions
+    elif partitions not in all_partitions:
+        sys.exit('Partition "' + partitions + '" not a valid partition. Must be one of: "' + '", "'.join(all_partitions) + '"')
     
+    partstats = []
+    for partition in partitions:
+        partstats.append(psutil.disk_usage(partition))
+    partstats = pd.DataFrame(partstats, index=partitions)
+    print(partstats)
+    print(partstats.applymap(humanize))
+
+
+def humanize(num_bytes):
+    '''
+    Convert an arbitrary number of bytes to human-readable form.
+    '''
+    for factor in reversed(range(len(sizes))):
+        converted = round(num_bytes / 1024 ** factor, 1)
+        if converted >= 1:
+            return re.sub(r'\.0', '', str(converted) + sizes[factor])
+
+
+def dehumanize(byte_string):
+    '''
+    Convert a humanized bytestring (eg. 18.5TB) to number of bytes
+    '''
+    byte_string = str(byte_string)  # just in case
+    factor = 0
+    for scale in reversed(range(len(sizes))):
+        if sizes[scale] in byte_string:
+            factor = scale
+            break       
+    number = float(re.findall(r'[0-9]+\.?[0-9]*', byte_string)[0])
+    return int(number * 1024 ** factor)
+
 
 if __name__ == '__main__':
     main()
